e5dbf43d96792944cb17caa7b44534ff777666c6
Mihail <mihai.iurcu1@gmail.com>
2023-07-21 16:16:50 +0300

    3

--- todo-backend/src/main/java/md/tekwill/todo/model/Task.java
+++ todo-backend/src/main/java/md/tekwill/todo/model/Task.java
@@ -1,11 +1,9 @@
 package md.tekwill.todo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
